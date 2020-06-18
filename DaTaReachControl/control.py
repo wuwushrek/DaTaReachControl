467b234ede879ce764b90ee0145680439ece2f59
wuwushrek <frmbouwe@gmail.com>
2020-06-18 09:40:10 -0500

    add fast gurobi solver

--- DaTaReachControl/control.py
+++ DaTaReachControl/control.py
@@ -7,6 +7,7 @@ from DaTaReachControl import FOverApprox, GOverApprox
 from DaTaReachControl import Interval
 
 import cvxpy as cp
+import gurobipy as gp
 
 def computeBounds(bj, Aj):
     """
@@ -70,6 +71,7 @@ def computeAffineLinear(x, dt, fOver, GOver, uRange, dtCoeff):
                     np.matmul(np.tensordot(JG, uRange, axes=([1,0]))[:,:,0]+Jf,
                     GSi))* dt_2
     return bi, Ai
+
 
 def computeOptimisticProblem(Al, Au, bl, bu, uRange, costFun, learningConstr=None):
     """Generate the 2^q convex optimization problems that need to be solve
@@ -154,6 +156,97 @@ def solveProblems(problem, u_var, cost_var, solver=cp.GUROBI, verbose=True,
     costValue = np.array([cost.value for cost in cost_var])
     ind = np.argmin(costValue)
     return u_var[ind].value, costValue[ind]
+
+def gurobiControlProblem(bl, Al, uRange, costFun, learningConstr):
+    """Compute an initial gurobi model that finds the near optimal
+    control value"""
+    listConstr = [dict()]
+    for i in range(Al.shape[1]):
+        if uRange[i,0].lb >= 0 or uRange[i,0].ub <= 0:
+            for d in listConstr:
+                d[i] = uRange[i,0].lb >= 0
+            continue
+        list_p = copy.deepcopy(listConstr)
+        for d in list_p:
+            d[i] = True
+        list_n = copy.deepcopy(listConstr)
+        for d in list_n:
+            d[i] = False
+        listConstr = list_p + list_n
+
+    # Create the optimization problem
+    counterPb = 0
+    dictConstr = dict()
+    dictU = dict()
+    listCost = []
+
+    # Create a new model
+    mOpt = gp.Model("noptimal")
+
+    for d in listConstr:
+        u = [mOpt.addVar(lb=-gp.GRB.INFINITY, name='u_{}_{}'.format(counterPb,i))\
+                 for i in range(Al.shape[1])]
+        x_var = [mOpt.addVar(lb=-gp.GRB.INFINITY, name='x_{}_{}'.format(counterPb,i))\
+                     for i in range(Al.shape[0])]
+        # Constraints on teh variable x_var
+        for i, xi in enumerate(x_var):
+            coeffSup = list()
+            coeffInf = list()
+            for j in range(Al.shape[1]):
+                if d[j]:
+                    coeffSup.append(Al[i,j].ub)
+                    coeffInf.append(Al[i,j].lb)
+                else:
+                    coeffSup.append(Al[i,j].lb)
+                    coeffInf.append(Al[i,j].ub)
+            dictConstr[(counterPb,i)] = \
+                (mOpt.addLConstr(gp.LinExpr(coeffSup + [-1], u + [xi]),
+                    gp.GRB.GREATER_EQUAL, -bl[i,0].ub, name='Up_{}_{}'.format(counterPb,i)),
+                mOpt.addLConstr(gp.LinExpr(coeffInf + [-1], u + [xi]),
+                    gp.GRB.LESS_EQUAL, -bl[i,0].lb,name='Lo_{}_{}'.format(counterPb,i)))
+        # Constraints on the control variable u
+        for j, uj in enumerate(u):
+            if d[j]:
+                uj.lb = np.maximum(0,uRange[j,0].lb)
+                uj.ub = np.maximum(0,uRange[j,0].ub)
+            else:
+                uj.ub = np.minimum(0, uRange[j,0].ub)
+                uj.lb = np.minimum(0, uRange[j,0].lb)
+            if learningConstr is not None:
+                dictConstr[(counterPb,-j-1)] = \
+                    mOpt.addConstr(gp.LinExpr([learningConstr[j]], [uj]),
+                        gp.GRB.EQUAL, 0, name='B_{}_{}'.format(counterPb,j))
+        dictU[counterPb] = (u,d)
+        listCost.append(costFun(x_var,u))
+        counterPb = counterPb + 1
+    mOpt.setObjective(gp.quicksum(listCost))
+    return dictU, dictConstr, listCost, mOpt
+
+def solveGBproblem(bl, Al, learningConstr, dictU, dictConstr,
+                    listCost, mOpt, useMs=False, verbose=True):
+    for nbProblem, (u, d) in dictU.items():
+        for i in range(bl.shape[0]):
+            (c1, c2) = dictConstr[(nbProblem,i)]
+            c1.RHS = -bl[i,0].ub
+            c2.RHS = -bl[i,0].lb
+            for j, uj in enumerate(u):
+                if d[j]:
+                    mOpt.chgCoeff(c1, uj, Al[i,j].ub)
+                    mOpt.chgCoeff(c2, uj, Al[i,j].lb)
+                else:
+                    mOpt.chgCoeff(c1, uj, Al[i,j].lb)
+                    mOpt.chgCoeff(c2, uj, Al[i,j].ub)
+        # Constraints on the control variable u
+        for j, uj in enumerate(u):
+            if learningConstr is not None:
+                c3 = dictConstr[(nbProblem,-j-1)]
+                mOpt.chgCoeff(c3, uj, learningConstr[j])
+    mOpt.Params.OutputFlag = 0
+    mOpt.optimize()
+    costVal = np.array([obj.getValue() for obj in listCost])
+    ind = np.argmin(costVal)
+    return np.array([uVal.x for uVal in dictU[ind][0]]), costVal[ind]
+
 
 class DaTaControl:
     """
@@ -349,6 +442,205 @@ class DaTaControl:
                             self.listCost, solver=self.solver,
                             verbose=self.optVerb, warm_start=True,
                             solopts=self.solopts)
+
+        if self.solverVerb:
+            print('Optimal cost : ', optCost)
+            print('Mean trajectory? ', optCost < self.thresMeanTraj)
+            print('Update over-approximations: ', self.updateMeas, self.indexUpdate)
+
+        # Do some logging
+        self.currentU = uOpt.reshape(-1,1)
+        self.nextStateOverApprox =  bj + np.matmul(Aj, self.currentU)
+
+        if self.solverVerb:
+            print('Next state overapprox: ', self.nextStateOverApprox.flatten())
+
+        return self.currentU
+
+
+class DaTaControlGurobi:
+    """
+    Main class for the systhesis of a 1-step "optimal" control in the sense
+    defined in the paper for unknown dynamical systems based on a finite-horizon
+    of a single trajectory
+
+    """
+    def __init__(self, cost_fun, uRange, lipF, lipG, traj={}, nDepf={}, bf={}, bGf={},
+                knownFunF={}, nDepG={}, bG={}, bGG={}, knownFunG={}, optVerb=False,
+                solverVerb=True, solver=cp.GUROBI, solopts={}, probLearning=[],
+                threshUpdateApprox=0.1, thresMeanTraj=1e-8, coeffLearning=0.1,
+                minDataF=15 , maxDataF=25, minDataG=5, maxDataG=10, dt=0.01):
+
+        self.minDataF = minDataF
+        self.maxDataF = maxDataF
+        self.minDataG = minDataG
+        self.maxDataG = maxDataG
+        self.nState = lipF.shape[0]
+        self.nControl = lipG.shape[1]
+
+        # self
+        # self.current_x = np.copy(initXtraj[:,-1])
+        # self.current_u = np.copy(uSeq[:,-1])
+
+        self.optVerb = optVerb
+        self.solverVerb = solverVerb
+        self.solver = solver
+        self.solopts = solopts
+
+        self.threshUpdateApprox = threshUpdateApprox
+        self.thresMeanTraj = thresMeanTraj
+        self.coeffLearning = coeffLearning
+        self.labLearning = np.array([-1]+[i for i in range(self.nControl)])
+        self.probLearning = np.full(self.nControl+1, 1.0/(self.nControl+1)) \
+                                if len(probLearning)==0 else probLearning
+
+        self.dt = dt
+        self.uRange = uRange
+        self.indexUpdate = -1
+        self.updateMeas = False
+
+        # Update cost function and create underlying optimizations problems
+        self.updateCost(cost_fun)
+
+        # Create and update the over-approximating functions
+        self.fover = FOverApprox(lipF, traj, nDepf, bf, bGf, knownFunF)
+        self.gover = GOverApprox(lipG, self.fover, traj, nDepG, bG, bGG, knownFunG)
+        self.updateLipOverapprox(lipF, lipG)
+        self.noInitData = not (self.fover.canApproximate() and self.gover.canApproximate())
+
+        # Update the cost scaling coefficient of gronwall
+        self.updateScalingGronwall()
+
+        self.nextStateOverApprox = None
+        self.currentX = None
+        self.currentU = None
+
+    def updateCost(self, costFun):
+        """
+        Create the optimizations problems that are going to be used for
+        the synthesis of a controller
+        """
+        self.dictU, self.dictConstr, self.listCost, self.mOpt = \
+            gurobiControlProblem(np.full((self.nState,1),Interval(0)),
+                np.full((self.nState,self.nControl),Interval(0)),
+                self.uRange, costFun, np.zeros(self.nControl))
+
+    def updateLipOverapprox(self, lipF, lipG):
+        """Update the Lipschite bounds of the over-approximatiosn"""
+        self.fover.Lf = lipF
+        self.gover.LG = lipG
+        self.updateScalingGronwall()
+
+    def updateScalingGronwall(self):
+        """Compute the coefficient coeffDt needed to returning the
+        a priori enclosure by gronwall theorem"""
+        uAbs = np.abs(self.uRange)
+        uSup = np.array([[uAbs[i,0].ub] for i in range(uAbs.shape[0])])
+        beta_dt = np.linalg.norm(self.fover.Lf + np.matmul(self.gover.LG , uSup))
+        self.coeffDt = (self.dt/(1-np.sqrt(self.nState)*self.dt*beta_dt))
+
+    def noInitialData(self):
+        if not (self.fover.canApproximate() and self.gover.canApproximate()):
+            self.indexUpdate = np.random.choice(self.labLearning,
+                                    p=self.probLearning)
+            self.currentU = np.zeros((self.nControl,1))
+            if self.indexUpdate != -1:
+                while True:
+                    self.currentU[self.indexUpdate,0] = self.coeffLearning * \
+                            np.random.uniform(self.uRange[self.indexUpdate,0].lb,
+                                              self.uRange[self.indexUpdate,0].ub)
+                    if not (np.abs(self.currentU[self.indexUpdate,0]) < 1e-8):
+                        break
+        else:
+            self.noInitData = False
+
+    def shouldUpdateTraj(self, nextState):
+        """
+        Check if the over-approxmations of f and G need to be updated. In case
+        no data has been given, we need to learn the dynamics locally
+        """
+        if self.nextStateOverApprox is None:
+            return False
+        distVal = np.linalg.norm(self.nextStateOverApprox-nextState)
+        return (distVal.ub-distVal.lb) > self.threshUpdateApprox
+
+    def synthControlUpdate(self):
+        """
+        When updating over-approximations of f and G, impose constraints
+        on the optimal control value such that it's either 0 or all
+        components are equal to 0 except from one.
+        """
+        self.learningConstr = np.full(self.nControl, 1)
+        self.indexUpdate = np.random.choice(self.labLearning, p=self.probLearning)
+        if self.indexUpdate != -1:
+            self.learningConstr[self.indexUpdate] = 0
+
+    def updateOverapprox(self, xVal, xDotVal, uVal, currTime=None):
+        """
+        Update our over-approximation of f and G when synth_control_update
+        has been executed. The update are done
+        when the over-approximation diameter is large than the threshold
+        threshUpdateApprox
+        """
+        if self.indexUpdate == -1:
+            self.fover.update(xVal, xDotVal, uVal, currTime)
+            self.fover.removeData(xVal, self.minDataF, self.maxDataF)
+        else:
+            self.gover.update(xVal, xDotVal, uVal, currTime)
+            self.gover.removeData(xVal, self.minDataG, self.maxDataG)
+
+    def __call__(self, currX, currXdot,t=None):
+        """
+        Function to call when trying to obtain the one-step optimal control
+        to minimize the distance to the setpoint. This function is assumed
+        to be called every dt
+        """
+        currXInt = np.array([[Interval(currX[i,0])] for i in range(self.nState)])
+
+        # print ('No init Data : ', self.noInitData)
+        if self.noInitData:
+            if self.currentX is not None:
+                self.updateOverapprox(self.currentX, currXdot, self.currentU,
+                                        None if t is None else t-self.dt)
+            self.currentX = currX
+            self.noInitialData()
+            return self.currentU
+
+        # Check if an update on f and G was done on the previous step
+        if self.updateMeas:
+            self.updateOverapprox(self.currentX, currXdot, self.currentU)
+
+        # Compute interval coefficients bj and Aj
+        bj, Aj = computeAffineLinear(currXInt, self.dt, self.fover,
+                            self.gover, self.uRange, self.coeffDt)
+
+        # No addditional constraint on the control
+        self.learningConstr = np.zeros(self.nControl)
+
+        # Check if we need to update f and G
+        self.updateMeas = self.shouldUpdateTraj(currX)
+
+        # Do some printing if required
+        if self.solverVerb:
+            nDataPoint = self.fover.E0x.shape[1] +\
+                sum([val[0].shape[1] for x, val in self.gover.Ej.items()])
+            print('No. of data points: ', nDataPoint)
+
+        # Synthesis of the controller
+        if self.updateMeas:
+            self.synthControlUpdate()
+            self.currentX = currX
+
+        # Synthesize control
+        uOpt, optCost = solveGBproblem(bj, Aj, self.learningConstr, self.dictU,
+                            self.dictConstr, self.listCost, self.mOpt)
+
+        # # Use the mean trajectory when we are very close to the target
+        # if optCost < self.thresMeanTraj:
+        #     uOpt, optCost = solveProblems(self.pbMid, self.dictU,
+        #                     self.listCost, solver=self.solver,
+        #                     verbose=self.optVerb, warm_start=True,
+        #                     solopts=self.solopts)
 
         if self.solverVerb:
             print('Optimal cost : ', optCost)
