356fbba4fbfeca9d3773f8a070f15561d5df8496
wuwushrek <frmbouwe@gmail.com>
2020-07-29 19:09:36 -0500

    multMs fix last commit

--- DaTaReachControl/intervalN.py
+++ DaTaReachControl/intervalN.py
@@ -10,16 +10,7 @@ from numpy import float64 as realN
 epsTolInt = 1e-6
 
 ########################################################################
-@jit([types.UniTuple(real,2)(real, real, real, types.misc.Omitted(None)),\
-      types.UniTuple(real,2)(real, real, real, real),\
-      types.UniTuple(real[:],2)(real[:], real[:], real, types.misc.Omitted(None)),\
-      types.UniTuple(real[:],2)(real[:], real[:], real[:], types.misc.Omitted(None)),\
-      types.UniTuple(real[:],2)(real[:], real[:], real, real),\
-      types.UniTuple(real[:],2)(real[:], real[:], real[:], real[:]),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real, types.misc.Omitted(None)),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real, real),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real[:,:], real[:,:])],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def add_i(x_lb, x_ub, y_lb, y_ub=None):
     """ Define the addition between an interval vector/matrix x=(x_lb,x_ub) and
         either a scalar y_lb or an interval vector/matrix  y = (y_lb, y_ub)"""
@@ -32,16 +23,7 @@ def add_i(x_lb, x_ub, y_lb, y_ub=None):
 ########################################################################
 
 ########################################################################
-@jit([types.UniTuple(real,2)(real, real, real, types.misc.Omitted(None)),\
-      types.UniTuple(real,2)(real, real, real, real),\
-      types.UniTuple(real[:],2)(real[:], real[:], real, types.misc.Omitted(None)),\
-      types.UniTuple(real[:],2)(real[:], real[:], real[:], types.misc.Omitted(None)),\
-      types.UniTuple(real[:],2)(real[:], real[:], real, real),\
-      types.UniTuple(real[:],2)(real[:], real[:], real[:], real[:]),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real, types.misc.Omitted(None)),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real, real),\
-      types.UniTuple(real[:,:],2)(real[:,:], real[:,:], real[:,:], real[:,:])],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def sub_i(x_lb, x_ub, y_lb, y_ub=None):
     """ Define the substraction between an interval x=(x_lb,x_ub) and
         either a scalar y_lb or an interval  y = (y_lb, y_ub)"""
@@ -54,10 +36,7 @@ def sub_i(x_lb, x_ub, y_lb, y_ub=None):
 ########################################################################
 
 ########################################################################
-@jit([types.UniTuple(real,2)(real,real,real),\
-      types.UniTuple(real[:],2)(real[:],real[:],real),\
-      types.UniTuple(real[:,:],2)(real[:,:],real[:,:],real)],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_i_scalar(x_lb, x_ub, val):
     """Define the multiplication between an interval x=(x_lb,x_ub) and a
        scalar given by val
@@ -67,8 +46,7 @@ def mul_i_scalar(x_lb, x_ub, val):
     else:
         return x_lb * val, x_ub * val
 
-@jit([types.UniTuple(real[:],2)(real[:],real[:],real[:])],\
-      nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_iv_sv(x_lb, x_ub, val):
     """Define the elementwise multiplication between an interval vector
        and a scalar vector """
@@ -78,8 +56,7 @@ def mul_iv_sv(x_lb, x_ub, val):
         res_lb[i], res_ub[i] = mul_i_scalar(x_lb[i], x_ub[i], val[i])
     return res_lb, res_ub
 
-@jit([types.UniTuple(real,2)(real,real,real,real)],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_i(x_lb, x_ub, y_lb, y_ub):
     """Define the multiplication between an interval x=(x_lb,x_ub) and an
        interval given by y = (y_lb, y_ub)
@@ -91,19 +68,19 @@ def mul_i(x_lb, x_ub, y_lb, y_ub):
     return np.minimum(val_1, np.minimum(val_2, np.minimum(val_3,val_4))),\
            np.maximum(val_1, np.maximum(val_2, np.maximum(val_3,val_4)))
 
-@jit([types.UniTuple(real,2)(real,real,real)],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_i_0c(x_lb, x_ub, val):
     """ Define the multiplication between an interval x=(x_lb,x_ub) and the
         interval (0,val) wher eval >=0 """
-    if x_lb >= 0:
+    if np.abs(x_lb-x_ub) <= epsTolInt:
+        x_lb = x_ub
+    if x_lb >= epsTolInt:
         return 0.0, val*x_ub
-    if x_ub <= 0:
+    if x_ub <= -epsTolInt:
         return val* x_lb, 0.0
     return val*x_lb, val*x_ub
 
-@jit([types.UniTuple(real,2)(real,real,real)],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_i_lip(x_lb, x_ub, val):
     """ Define the multiplication between an interval vector x=(x_lb,x_ub) and the
         interval (-val,val) where val >=0
@@ -111,8 +88,7 @@ def mul_i_lip(x_lb, x_ub, val):
     return np.minimum(-val*x_ub, val*x_lb), np.maximum(-val*x_lb, val*x_ub)
 
 
-@jit([types.UniTuple(real[:],2)(real[:],real[:],real)],\
-      nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_iv_0c(x_lb, x_ub, val):
     """ Define the multiplication between an interval vector x=(x_lb,x_ub) and the
         interval (0,val) where val >=0 """
@@ -122,8 +98,7 @@ def mul_iv_0c(x_lb, x_ub, val):
         res_lb[i], res_ub[i] = mul_i_0c(x_lb[i], x_ub[i], val)
     return res_lb, res_ub
 
-@jit([types.UniTuple(real[:],2)(real[:,:],real[:,:],real[:], real[:])],\
-      nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_iMv(x_lb, x_ub, y_lb, y_ub):
     """ Define the multiplication between an interval Matrix x=(x_lb,x_ub) and an
         interval vector y = (y_lb, y_ub)
@@ -140,8 +115,24 @@ def mul_iMv(x_lb, x_ub, y_lb, y_ub):
         res_ub[i] = res_i_ub
     return res_lb, res_ub
 
-@jit([types.UniTuple(real[:,:],2)(real[:,:,:],real[:,:,:],real[:], real[:])],\
-      nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
+def mul_Ms_i(x_lb, x_ub, y):
+    """ Define the multiplication between an interval Matrix x=(x_lb,x_ub) and
+        a scalar vector y
+    """
+    res_lb = np.empty(x_lb.shape[0], dtype=realN)
+    res_ub = np.empty(x_lb.shape[0], dtype=realN)
+    for i in prange(x_lb.shape[0]):
+        res_i_lb, res_i_ub = 0 , 0
+        for k in prange(x_lb.shape[1]):
+            t_lb, t_ub = mul_i_scalar(x_lb[i,k], x_ub[i,k], y[k])
+            res_i_lb += t_lb
+            res_i_ub += t_ub
+        res_lb[i] = res_i_lb
+        res_ub[i] = res_i_ub
+    return res_lb, res_ub
+
+@jit(nopython=True, parallel=False, fastmath=True)
 def mul_iTv(x_lb, x_ub, y_lb, y_ub):
     """ Define the multiplication between an interval Tensor x=(x_lb,x_ub) and an
         interval vector y = (y_lb, y_ub)
@@ -152,7 +143,7 @@ def mul_iTv(x_lb, x_ub, y_lb, y_ub):
         for k in prange(x_lb.shape[2]):
             res_i_lb, res_i_ub = 0 , 0
             for l in prange(x_lb.shape[1]):
-                if x_lb[i,l,k] == -x_ub[i,l,k]:
+                if np.abs(x_lb[i,l,k] + x_ub[i,l,k]) <= epsTolInt:
                     t_lb, t_ub  = mul_i_lip(y_lb[l], y_ub[l], x_ub[i,l,k])
                 else:
                     t_lb, t_ub = mul_i(x_lb[i,l,k], x_ub[i,l,k], y_lb[l], y_ub[l])
@@ -184,8 +175,7 @@ def mul_iTv(x_lb, x_ub, y_lb, y_ub):
 ########################################################################
 
 ########################################################################
-@jit(types.UniTuple(real,2)(real, real, real, real),\
-     nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def div_i(x_lb, x_ub, y_lb, y_ub):
     """ Define the division between two intervals """
     assert y_lb > 0 or y_ub < 0
@@ -218,9 +208,7 @@ def div_i(x_lb, x_ub, y_lb, y_ub):
 ########################################################################
 
 #########################################################################
-@jit([types.UniTuple(real,2)(real, real),
-      types.UniTuple(real[:],2)(real[:], real[:])],\
-      nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def sqrt_i(x_lb, x_ub):
     """ Compute the square root of an interval or interval vector """
     return np.sqrt(x_lb), np.sqrt(x_ub)
@@ -267,9 +255,7 @@ def cos_i(x_lb, x_ub):
 ########################################################################
 
 ########################################################################
-@jit([types.UniTuple(real,2)(real, real)],\
-      # types.UniTuple(real[:],2)(real[:], real[:])],\
-        nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def sin_i(x_lb, x_ub):
     """ Compute the sinus of an interval """
     nx_lb, nx_ub = sub_i(x_lb, x_ub, np.pi/2)
@@ -277,9 +263,7 @@ def sin_i(x_lb, x_ub):
 ########################################################################
 
 ########################################################################
-@jit([types.UniTuple(real,2)(real, real)],\
-      # types.UniTuple(real[:],2)(real[:], real[:])],\
-        nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def tan_i(x_lb, x_ub):
     """ Compute the tan of an interval """
     sx_lb, sx_ub = sin_i(x_lb, x_ub)
@@ -288,39 +272,43 @@ def tan_i(x_lb, x_ub):
 ########################################################################
 
 ########################################################################
-@jit(types.UniTuple(real,2)(real, real, real, real),\
-     nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def and_i(x_lb, x_ub, y_lb, y_ub):
     """ Compute the intersection of two intervals"""
     lb = np.maximum(x_lb,y_lb)
     ub = np.minimum(x_ub,y_ub)
-    assert lb <= ub
+    diff = ub-lb
+    if np.abs(diff) <= epsTolInt:
+        return lb, lb
+    assert diff >= epsTolInt
     return lb, ub
 
-@jit(types.UniTuple(real,2)(real[:], real[:]),\
-     nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def and_iv(x_lb, x_ub):
     """ Compute the intersection of all intervals in a vector"""
     lb = np.max(x_lb)
     ub = np.min(x_ub)
-    assert lb <= ub
+    diff = ub-lb
+    if np.abs(diff) <= epsTolInt:
+        return lb, lb
+    assert diff >= epsTolInt
     return lb, ub
 
 ########################################################################
 
 ########################################################################
-@jit(types.UniTuple(real,2)(real, real),\
-     nopython=True, parallel=False, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def pow2_i(x_lb, x_ub):
     """ COmpute x^2 for the given interval x=(x_lb, x_ub)"""
-    if x_ub <= 0:
+    if np.abs(x_ub-x_lb) <= epsTolInt:
+        return x_ub**2, x_ub**2
+    if x_ub <= -epsTolInt:
         return x_ub**2, x_lb**2
-    if x_lb >= 0:
+    if x_lb >= epsTolInt:
         return x_lb**2, x_ub**2
     return 0.0, np.maximum(x_lb**2,x_ub**2)
 
-@jit(types.UniTuple(real,2)(real[:], real[:]),\
-     nopython=True, parallel=True, fastmath=True)
+@jit(nopython=True, parallel=False, fastmath=True)
 def norm_i(x_lb, x_ub):
     """ COmpute the norm of the interval vector given by x_lb and x_ub"""
     res_lb = 0
@@ -332,6 +320,20 @@ def norm_i(x_lb, x_ub):
     return sqrt_i(res_lb, res_ub)
 ########################################################################
 
+########################################################################
+@jit(nopython=True, parallel=False, fastmath=True)
+def abs_i(x_lb, x_ub):
+    res = np.empty(x_lb.shape[0], dtype=realN)
+    for i in prange(x_ub.shape[0]):
+        res[i] = np.maximum(np.abs(x_lb[i]), np.abs(x_ub[i]))
+    return res
+
+@jit(nopython=True, parallel=False, fastmath=True)
+def contains_i(x_lb, x_ub, y_lb, y_ub):
+    if np.abs(y_ub-y_lb) <= epsTolInt:
+        y_lb = y_ub
+    return x_lb-y_lb <= epsTolInt and x_ub-y_ub >= -epsTolInt
+########################################################################
 
 # Predefined types of the Interval
 spec = [('lb', real), ('ub', real)]
